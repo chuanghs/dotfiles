7972de5ae0d06dbce34f9944d895c5b76f9d7f22
Zombie <chuanghs@gmail.com>
2025-07-26 11:54:46 +0800

    add telescope, and set leaderkey to space

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,6 +4,7 @@ local opts = {
     silent = true,       -- do not show message
 }
 
+vim.g.mapleader = " "
 -----------------
 -- Normal mode --
 -----------------
@@ -14,6 +15,7 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
 vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
 vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+
 
 -- Resize with arrows
 -- delta: 2 lines
