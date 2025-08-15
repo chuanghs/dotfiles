7db8fa647569238878553d15dcdb3d7acf1b0ffc
Zombie <chuanghs@gmail.com>
2025-08-16 00:37:22 +0800

    complete orgmode config for nvim

--- nvim/lua/plugins/org-mode.nvim.lua
+++ nvim/lua/plugins/org-mode.nvim.lua
@@ -5,9 +5,13 @@ return {
 	config = function()
 		-- Setup orgmode
 		require('orgmode').setup({
-			org_agenda_files = '~/orgfiles/**/*',
-			org_default_notes_file = '~/orgfiles/inbox.org',
+			org_agenda_files = '~/orgfiles/**/*.org',
+			org_default_notes_file = '~/orgfiles/journal.org',
 			org_todo_keywords = { 'NA', 'WAITING', '|', 'DONE', 'CANCEL'},
+			org_capture_templates = {
+				t = { description = 'Task', headline="Inbox", template = '* NA %?\n  %u', target = "~/orgfiles/journal.org"},
+				j = { description = 'Journal Entry', template = "* %U\n%?", target="~/orgfiles/journal.org", datetree=true}
+			}
 		})
 
 		-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
